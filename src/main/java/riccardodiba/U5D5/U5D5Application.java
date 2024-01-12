25e95c706fd132a4b679cf7c1dcd8f71d2c0cd82
Riccardo Di Bari <riccardodibari@hotmail.it>
2024-01-12 14:34:42 +0100

    commit

---
+++ src/main/java/riccardodiba/U5D5/U5D5Application.java
@@ -0,0 +1,14 @@
+package riccardodiba.U5D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5D5Application.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/riccardodiba/U5D5/U5D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package riccardodiba.U5D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
